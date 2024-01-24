432431d4b1707298c78b231eac7098200b139c4e
september1993 <156948222+september1993@users.noreply.github.com>
2024-01-24 18:26:37 +0000

    M1HW1 Done

---
+++ M1HW1_Mattocks.cpp
@@ -0,0 +1,27 @@
+/*
+CSC 134
+M1HW1 - Movie Talk
+I Mattocks
+1/24/2024
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int release_year = 2005;
+    double yen_grossed_billions = 1.8;
+    string movie_title = "Arashi no Yoru ni";
+
+    cout << "The title of the movie I'll be talking about is: " << movie_title << "." << endl;
+    cout << "It is an animated film that released in " << release_year << "." << endl;
+    cout << "During its run in theatres, the movie grossed a total of " << yen_grossed_billions << " billion yen." << endl;
+
+    cout << endl;
+
+    cout << "The plot revolves around an unlikely friendship between Mei, a goat, and Gabu, a wolf." << endl;
+    cout << "It received several adaptations for radio, television, stage, and even for Nintendo DS." << endl;
+    cout << "After leaving their respective families to escape persecution, Mei assures Gabu that \"there's a green forest somewhere out there where we can live freely.\"" << endl;
+    cout << "Stuck in a blizzard during their search for the forest, Mei tells Gabu: \"Live enough for the both of us.\"" << endl;
+
+}
